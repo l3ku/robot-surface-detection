b930829665ab5684168ba5728cd937e4ef9d3529
Leo Toikka <leo@seravo.fi>
2019-01-24 17:43:33 +0200

    Divide samples to blocks and perform division to train, test and validate

--- main.py
+++ main.py
@@ -2,15 +2,35 @@ import numpy as np
 import pandas as pd
 from pprint import pprint
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
+from sklearn import preprocessing, model_selection
 
 train_data = np.load('X_train_kaggle.npy')
 all_id_classes = pd.read_csv('y_train_final_kaggle.csv')
+groups = pd.read_csv('groups.csv')
 
 le = preprocessing.LabelEncoder()
 le.fit(all_id_classes['Surface'])
 all_id_classes_transformed = le.transform(all_id_classes['Surface'])
 all_id_classes['Surface'] = all_id_classes_transformed
+
+blocks_list = [list() for _ in range(36)]
+blocks_classes_list = [-1 for _ in range(36)]
+
+for value in groups.values:
+	id = value[0]
+	group_id = value[1]
+	surface = value[2]
+	blocks_list[group_id].append(train_data[id])
+	if blocks_classes_list[group_id] == -1:
+		blocks_classes_list[group_id] = surface
+
+X_train, X_validation, y_train_str, y_validation_str = model_selection.train_test_split(blocks_list, blocks_classes_list, test_size=0.2)
+y_train = le.transform(y_train_str)
+y_validation = le.transform(y_validation_str)
+
+ss = model_selection.ShuffleSplit()
+test_indexes = np.array(list(ss.split(X_train)))
+
 
 # Checking the amount of data given
 # Most likely not useful in the actual solution, just visualizing the data
